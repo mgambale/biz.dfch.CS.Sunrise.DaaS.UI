481e347277118f41f0d40a46232bd4e234c98377
Ronald Rink <ronald.rink@d-fens.net>
2015-09-04 13:34:20 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # biz.dfch.CS.Sunrise.DaaS.UI
-LightSwitch HTML User interface for Sunrise DaaS framnework
+LightSwitch HTML User interface for Sunrise DaaS framework
---
+++ src/biz.dfch.CS.Sunrise.DaaS.UI/biz.dfch.CS.Sunrise.DaaS.UI.HTMLClient/GeneratedArtifacts/viewModel.intellisense.js
@@ -0,0 +1,48 @@
+﻿/// <reference path="viewModel.js" />
+
+(function (lightSwitchApplication) {
+
+    var $element = document.createElement("div");
+
+    lightSwitchApplication.Home.prototype._$contentItems = {
+        Tabs: {
+            _$class: msls.ContentItem,
+            _$name: "Tabs",
+            _$parentName: "RootContentItem",
+            screen: lightSwitchApplication.Home
+        },
+        Group: {
+            _$class: msls.ContentItem,
+            _$name: "Group",
+            _$parentName: "Tabs",
+            screen: lightSwitchApplication.Home,
+            data: lightSwitchApplication.Home,
+            value: lightSwitchApplication.Home
+        },
+        Popups: {
+            _$class: msls.ContentItem,
+            _$name: "Popups",
+            _$parentName: "RootContentItem",
+            screen: lightSwitchApplication.Home
+        }
+    };
+
+    msls._addEntryPoints(lightSwitchApplication.Home, {
+        /// <field>
+        /// Called when a new Home screen is created.
+        /// <br/>created(msls.application.Home screen)
+        /// </field>
+        created: [lightSwitchApplication.Home],
+        /// <field>
+        /// Called before changes on an active Home screen are applied.
+        /// <br/>beforeApplyChanges(msls.application.Home screen)
+        /// </field>
+        beforeApplyChanges: [lightSwitchApplication.Home],
+        /// <field>
+        /// Called after the Group content item has been rendered.
+        /// <br/>postRender(HTMLElement element, msls.ContentItem contentItem)
+        /// </field>
+        Group_postRender: [$element, function () { return new lightSwitchApplication.Home().findContentItem("Group"); }]
+    });
+
+}(msls.application));
